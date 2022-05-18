c8eae4465ae3e50d4ce1a9d0357799f0c7e8e9fa
Sriranjani Jayaprakash <sriranjani.jp@gmail.com>
2022-05-18 22:08:29 +0530

    18-05-22

---
+++ binsearch.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class binsearch
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int arr[] = {5,3,7,3,7,9,11,5,6,8,9};
+        int len = arr.length;
+        int big = 0;
+        int pos = 0;
+        for (int i = 0; i<len; i++)
+        {
+            if (arr[i]>big)
+            {
+                big = arr[i];
+                pos = i;
+            }
+        }
+        System.out.println("The biggest no is " + big);
+        System.out.println("Found at position " + pos);
+    }
+}
